b372d53d0a3f2a9af01d56c76cd34abd1ae7b430
Nathan Curtis <nathan@nathancurtis.to>
2026-06-18 14:34:57 -0700

    scaffold: fix WPF-UI package, FluentWindow, theme setup

--- App/App.xaml.cs
+++ App/App.xaml.cs
@@ -1,4 +1,7 @@
 using System.Windows;
+using System.Windows.Media;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
 
 namespace SizeMonitor;
 
@@ -7,6 +10,11 @@ public partial class App : Application
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
+
+        var accent = Color.FromArgb(0xFF, 0x4C, 0x9D, 0xFF);
+        ApplicationAccentColorManager.Apply(accent, ApplicationTheme.Dark, systemGlassColor: false, systemAccentColor: false);
+        ApplicationThemeManager.Apply(ApplicationTheme.Dark, WindowBackdropType.Mica, updateAccent: false);
+
         new MainWindow().Show();
     }
 }
--- App/MainWindow.xaml.cs
+++ App/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
-using System.Windows;
+using Wpf.Ui.Controls;
 
 namespace SizeMonitor;
 
-public partial class MainWindow : Window
+public partial class MainWindow : FluentWindow
 {
     public MainWindow()
     {
